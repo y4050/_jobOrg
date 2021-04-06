207c5ef1fdc20e847235755df911120a4ddbb53f
J <jchen_729@hotmail.com>
2021-04-06 09:50:08 -0700

    .

--- joborg/settings.py
+++ joborg/settings.py
@@ -11,11 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
